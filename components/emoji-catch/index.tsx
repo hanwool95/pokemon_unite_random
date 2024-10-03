cdb2a998845ebd041d22f495f2fd8aa124600eb5
hanwool95 <hanwool95@snu.ac.kr>
2024-10-03 16:29:52 +0900

    FEATURE: add room for chat

--- components/emoji-catch/index.tsx
+++ components/emoji-catch/index.tsx
@@ -3,26 +3,54 @@
 import useSockets from "@/hooks/useSockets";
 
 const EmojiCatchContainer = () => {
-    const { message, setMessage, messages, sendMessage } = useSockets()
+  const {
+    roomCode,
+    setRoomCode,
+    message,
+    setMessage,
+    messages,
+    sendMessage,
+    joinRoom,
+    createRoom,
+    joinedRoom,
+  } = useSockets();
 
-    return (
-        <div>
-            <h1>Catch Mind Game</h1>
-            <ul>
-                {messages.map((msg, index) => (
-                    <li key={index}>{msg}</li>
-                ))}
-            </ul>
-            <form onSubmit={sendMessage}>
-                <input
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    autoComplete="off"
-                />
-                <button type="submit">Send</button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Catch Mind Game</h1>
+      {joinedRoom ? (
+        <>
+          <h2>Room Code: {roomCode}</h2>
+          <ul>
+            {messages.map((msg, idx) => (
+              <li key={idx}>
+                {msg.sender}: {msg.message}
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={sendMessage}>
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              autoComplete="off"
+            />
+            <button type="submit">Send</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <button onClick={createRoom}>Create Room</button>
+          <input
+            type="text"
+            value={roomCode}
+            onChange={(e) => setRoomCode(e.target.value)}
+            placeholder="Enter room code"
+          />
+          <button onClick={joinRoom}>Join Room</button>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default EmojiCatchContainer
+export default EmojiCatchContainer;
--- hooks/useSockets.tsx
+++ hooks/useSockets.tsx
@@ -2,18 +2,35 @@ import React, { useCallback, useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const useSockets = () => {
+  const [roomCode, setRoomCode] = useState("");
   const [socket, setSocket] = useState<any>(null);
   const [message, setMessage] = useState<string>("");
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<
+    { sender: string; message: string }[]
+  >([]);
+  const [joinedRoom, setJoinedRoom] = useState(false);
 
   useEffect(() => {
     // 소켓 서버에 연결
     const skt = io("http://localhost:4000");
 
-    // 서버로부터 메시지를 받으면 messages 배열에 추가
-    skt.on("chat", (msg: string) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
+    skt.on("roomCreated", (code: string) => {
+      setRoomCode(code);
+      setJoinedRoom(true);
     });
+
+    skt.on("joinedRoom", (code: string) => {
+      setRoomCode(code);
+      setJoinedRoom(true);
+    });
+
+    skt.on(
+      "chat",
+      ({ sender, message }: { sender: string; message: string }) => {
+        setMessages((prev) => [...prev, { sender, message }]);
+      },
+    );
+
     setSocket(skt);
 
     // 컴포넌트가 언마운트 될 때 소켓 연결 해제
@@ -22,18 +39,37 @@ const useSockets = () => {
     };
   }, []);
 
+  const createRoom = useCallback(() => {
+    console.log("create room");
+    socket.emit("createRoom");
+  }, [socket]);
+
+  const joinRoom = useCallback(() => {
+    socket.emit("joinRoom", roomCode);
+  }, [socket, roomCode]);
+
   const sendMessage = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       if (message.trim() && socket) {
-        socket.emit("chat", message);
+        socket.emit("chat", { roomCode, message });
         setMessage("");
       }
     },
-    [message, socket],
+    [message, socket, roomCode],
   );
 
-  return { message, setMessage, messages, sendMessage };
+  return {
+    roomCode,
+    setRoomCode,
+    message,
+    setMessage,
+    messages,
+    sendMessage,
+    createRoom,
+    joinRoom,
+    joinedRoom,
+  };
 };
 
 export default useSockets;
