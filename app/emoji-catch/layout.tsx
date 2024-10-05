04fe336d6cffc297765c6288391b1d5f82fbe602
hanwool95 <hanwool95@snu.ac.kr>
2024-10-05 21:36:53 +0900

    FEATURE: 유튜브 버튼 추가

--- app/emoji-catch/layout.tsx
+++ app/emoji-catch/layout.tsx
@@ -22,6 +22,24 @@ export default function RootLayout({
           height={360}
         />
         {children}
+        <div className={"mx-auto"}>
+          <p className={"mx-auto text-center"}>
+            {"created by 3분 요약 카이사르"}
+          </p>
+          <p className={"mx-auto text-center mt-2 whitespace-pre-wrap"}>
+            {"이용료 = 구독!"}
+          </p>
+          <div className="flex justify-center mt-2">
+            <a
+              href="https://www.youtube.com/@3min-caesar"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
+            >
+              카이사르 유튜브 바로가기
+            </a>
+          </div>
+        </div>
       </body>
     </html>
   );
