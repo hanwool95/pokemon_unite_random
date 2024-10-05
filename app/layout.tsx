30911ec80a866da1e161485fa8f44b461013af3b
hanwool95 <hanwool95@snu.ac.kr>
2024-10-05 15:29:07 +0900

    FEATURE: favico 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "너즐록 섞기",
   description: "3분 요약 카이사르의 포유나 너즐록 섞어",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
